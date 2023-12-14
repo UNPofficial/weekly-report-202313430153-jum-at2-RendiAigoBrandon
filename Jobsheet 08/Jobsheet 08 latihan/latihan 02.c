00a1f3359a86fc002db391109bc452b0daebd517
RendiAigoBrandon <144934257+RendiAigoBrandon@users.noreply.github.com>
2023-12-14 17:15:01 +0700

    Add files via upload

---
+++ Jobsheet 08/Jobsheet 08 latihan/latihan 02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//create by 23343082_rendi aigo brandon
+
+// membuat fungsi say_hello()
+void say_hello(){
+    printf("Hello Selamat Datang! \n");
+}
+
+void main(){
+    // memanggil fungsi say_hello()
+    say_hello();
+    say_hello();
+    say_hello();
+}
