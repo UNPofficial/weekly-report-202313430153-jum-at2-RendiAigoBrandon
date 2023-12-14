e5ea0c091d03dcbc4c3df0ec7c14728de084f172
RendiAigoBrandon <144934257+RendiAigoBrandon@users.noreply.github.com>
2023-12-14 17:22:38 +0700

    Add files via upload

---
+++ Jobsheet 08/Jobsheet 08 latihan/latihan 03.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//create by 23343082_rendi aigo brandon
+
+void say_hello(char name[]){
+    printf("Hello %s! \n", name);
+}
+
+void main(){
+    say_hello("Dian");
+    say_hello("Petani");
+    say_hello( "Kode");
+}
