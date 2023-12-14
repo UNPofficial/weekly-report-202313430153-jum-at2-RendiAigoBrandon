6b422f4500594e06d4d2570d8ff09354cf75681a
RendiAigoBrandon <144934257+RendiAigoBrandon@users.noreply.github.com>
2023-12-14 16:56:13 +0700

    Add files via upload

---
+++ Jobsheet 03/Jobsheet 03 latihan/latihan 01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// Created by 23343082_Rendi aigo brandon
+
+
+int main(){
+	
+	int usia = 19;
+	float berat = 65.3;
+	double tinggi = 178.43;
+	char jenis_kelamin = 'L';
+	
+	printf("Usia : %i tahun\n", usia);
+	printf("Tinggi : %.2f cm\n", tinggi);
+	printf("Berat : %.2f Kg\n", berat);
+	printf("Jenis kelamin : %c\n", jenis_kelamin);
+	
+	return 0;
+	
+}
---
+++ Jobsheet 03/Jobsheet 03 latihan/latihan 02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// Created by 23343082_Rendi aigo brandon
+
+
+void main(){ 
+	
+	int a = 5;
+	int b = 2;
+	
+	float c = a / b;
+	
+	printf("Hasil a / b = %.2f\n", c);
+		
+}
