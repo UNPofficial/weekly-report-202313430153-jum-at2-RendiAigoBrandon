6aa7aba697837a37925d493df879e04223b3c5da
RendiAigoBrandon <144934257+RendiAigoBrandon@users.noreply.github.com>
2023-12-14 16:51:54 +0700

    Add files via upload

---
+++ Jobsheet 02/Jobsheet 02 latihan/latiahan 01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Created by 23343082_Rendi aigo brandon
+
+int main() {
+    
+    printf("Hello, ini adalah teks output\n");
+    printf("nama saya %s\n", "dian");
+    printf("usia saya %d\n", 20);
+    
+    return 0;
+    
+}
