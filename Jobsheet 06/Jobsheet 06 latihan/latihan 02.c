34c6d47175ea4100d28d15c5a21887e77bdd0a63
RendiAigoBrandon <144934257+RendiAigoBrandon@users.noreply.github.com>
2024-03-25 20:57:36 +0700

    Delete Jobsheet 06 directory

--- Jobsheet 06/Jobsheet 06 latihan/latihan 02.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-//create by 23343082_rendi aigo brandon   
-
-void main(){
-    for(int i = 0; 1 < 10; i++){
-        printf("Perulangan ke-%i\n", i);
-    }
-}
--- Jobsheet 06/Jobsheet 06 latihan/latihan 03.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-//create by 23343082_rendi aigo brandon  
-
-void main(){
-    for(int counter = 0; counter < 50; counter+=2){
-        printf("Perulangan ke-%i\n", counter);
-    }
-}
