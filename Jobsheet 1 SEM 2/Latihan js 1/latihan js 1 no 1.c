dc35d6d438545e9d725cb8ad00d35ee7f586554c
RendiAigoBrandon <144934257+RendiAigoBrandon@users.noreply.github.com>
2024-03-25 23:04:52 +0700

    Delete Jobsheet 1 SEM 2 directory

--- Jobsheet 1 SEM 2/Latihan js 1/latihan js 1 no 1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-//create by 23343082_rendi aigo brandon
-
-{
-printf("\n Welcome to the world of C");// prints the message on the screen
-return 0;// returns a value O to the operating system
-}
